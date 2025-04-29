7a78296c8446e845eec5c1b5d3e4c0fbf57576bd
katekiselevich <kiselkath@gmail.com>
2025-04-29 12:11:53 +0200

    Added testSortByGpa

---
+++ src/test/java/org/school/StudentTest.java
@@ -0,0 +1,33 @@
+package org.school;
+
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class StudentTest {
+
+    private List<Student> students;
+
+    @BeforeEach
+    void setUp(){
+        students = new ArrayList<>();
+        students.add(new Student("Bob", 2.8));
+        students.add(new Student("Alice", 3.9));
+        students.add(new Student("Diana", 4.0));
+        students.add(new Student("Charlie", 3.5));
+    }
+
+    @Test
+    @DisplayName("Students should sort by GPA descending")
+    void testSortByGpa(){
+        Collections.sort(students);
+        assertEquals("Diana",students.get(0).getName() );
+        assertEquals("Alice", students.get(1).getName());
+        assertEquals("Charlie", students.get(2).getName());
+        assertEquals("Bob", students.get(3).getName());
+    }
+
+}
